4e4f4c642cee10f9bc0d182c40de1d8d7dda8845
planeur <planeur903@gmail.com>
2018-02-21 20:58:15 +0000

    Add todo get/id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var bodyParser= require('body-parser');
-
+const {ObjectID}= require('mongodb');
 var {mongoose}= require("./db/mongoose");
 var {Todo}= require("./models/Todo");
 var {User}= require("./models/User");
@@ -25,6 +25,21 @@ app.get("/todos",(req,res)=>{
     },(e)=>{
         res.status(400).send(e);
     })
+})
+app.get("/todos/:id",(req,res)=>{
+    var id= req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(400).send();
+    }
+    Todo.findById(id).then((todo)=>{
+        if(todo){
+            
+            res.status(200).send(todo);
+        }else res.status(404).send();
+        
+    })/*.catch((e)=>{
+        res.status(400).send(e);
+    })*/
 })
 app.listen(3000,()=>{
     console.log('started on port 3000');
